0cfcf9ea720d7358168379f837f303ddfc77eec7
Rodion Vladimirov <r.vladimirov@outlook.com>
2022-12-14 12:26:01 +0300

    Edited project_database Added new methods

--- project_database.py
+++ project_database.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from pony.orm import *
 from datetime import *
 from CGRtools.files import SDFRead
+from pickle import dumps, loads
 
 db = Database()
 
@@ -54,6 +55,12 @@ class Molecule_Property(db.Entity):
     remaining = Optional(Decimal)
     shelf_life = Optional(datetime, auto=True)
 
+    def __init__(self, molecule, substance_form, remaining, shelf_life):
+        if Molecule_Property.exists(molecule=molecule, substance_form=substance_form, 
+                                    remaining=remaining, shelf_life=shelf_life):
+            raise ValueError('That property information is already in the database >:-(')
+        db.Entity.__init__(self, molecule=molecule, substance_form=substance_form, remaining=remaining, shelf_life=shelf_life)
+
     # Определяем функцию, которая показывает заканчивающиеся в ближайшие 180 дней сроки годности молекул
     def upcoming_180_days():
         with db_session:
@@ -72,6 +79,28 @@ class Molecule_Property(db.Entity):
                                 if molecule_property.shelf_life < datetime(year, month, day))
             return (list(results))         
 
+    # Определяем функцию, которая вернёт все молекулы определённой формы
+    def find_form(subst_form):
+        with db_session:
+            result = select((molecule, f'Форма: {molecule_property.substance_form}')
+                                for molecule in Molecule
+                                for molecule_property in molecule.property
+                                if molecule_property.substance_form == subst_form)
+            for i in list(result):
+                print(i)
+            return result
+    
+    # Определяем функцию, которая вернёт все молекулы меньше определённого порога по весу
+    def less_than(remain):
+        with db_session:
+            result = select((molecule, f'Остаток по весу: {molecule_property.remaining}')
+                                for molecule in Molecule
+                                for molecule_property in molecule.property
+                                if molecule_property.remaining < remain)
+            for i in list(result):
+                print(i)
+            return result
+    
 class Molecule_Structure(db.Entity):
     molecule = Required(Molecule)
     id = PrimaryKey(int, auto=True)  # Identification number of the structure
@@ -96,6 +125,9 @@ class Storage(db.Entity):
     place = Required(str)
     supplier = Optional(str)
 
+    def __init__(self, molecule, place='No information', supplier='No information'):
+        db.Entity.__init__(self, molecule=molecule, place=place, supplier=supplier)
+
 
 db.bind('sqlite', 'moleculesDB.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
@@ -104,5 +136,12 @@ data_molecules = []
 with SDFRead('logBB.sdf') as f:
     data_molecules = [r for r in f]
 
-Molecule.add_info(data_molecules[100], organic_molecule=False, substance_form='oil', remaining=None,
-                                            shelf_life='01.01.1900', place='No information', supplier='No information')
+with db_session:
+    # db.Molecule(data_molecules[9], organic_molecule=False, substance_form='Solution', remaining='360',
+    #                     shelf_life='15.06.2023', place='19', supplier='No information')
+
+    # db.Molecule[1].add_info(substance_form='Solution', remaining=500.0, shelf_life='01.01.2023')
+    # print(db.Molecule[1].structure.get_structure())
+    # print(type(db.Molecule[1]._structure()))
+    Molecule_Property.find_form('powder')
+    Molecule_Property.less_than(500)
