e650fae2f42e61e7dc1806920a012a33e1fa7eed
Rodion Vladimirov <r.vladimirov@outlook.com>
2022-12-19 16:56:26 +0300

    Refactoring

--- project_database.py
+++ project_database.py
@@ -3,6 +3,7 @@ from pony.orm import *
 from datetime import *
 from CGRtools.files import SDFRead
 from pickle import dumps, loads
+import time
 
 db = Database()
 
@@ -15,6 +16,7 @@ class Molecule(db.Entity):
     structure = Set('Molecule_Structure')
     properties = Set('Molecule_Property')
     storage = Set('Storage')
+    reactions = Set('MoleculeReaction')
 
     def __init__(self, structure, organic_molecule=True, substance_form='No information', remaining=None,
                         shelf_life='01.01.1900', place='No information', supplier='No information'):
@@ -31,18 +33,15 @@ class Molecule(db.Entity):
         return next(iter(self.structure)).structure
 
     @property
-    def molecule_description(self):
-        return next(iter(self.properties)).get_molecule_description
+    def description(self):
+        return [x.molecule_description for x in self.properties]
 
-    # Определяем метод, который передаём молекулу в каноничном виде и находим её в базе данных
-    def find_molecule_in_database(any_molecule):
-        with db_session:            
-            result = select(molecule
-                                for molecule in Molecule
-                                for molecule_structure in molecule.structure
-                                if molecule_structure.signature == bytes(any_molecule))
-            for i in result:
-                yield i
+    # Определяем метод, в который передаём молекулу в каноничном виде и находим её в базе данных
+    def find_molecule_in_db(any_molecule):
+        with db_session:
+            result =  Molecule_Structure.get(signature=bytes(any_molecule))
+            if result:
+                return result.molecule
 
     # Определяем метод, который обновляет информацию по имеющейся в базе данных молекуле
     def add_info(self, substance_form='No information', remaining=None,
@@ -64,51 +63,37 @@ class Molecule_Property(db.Entity):
         db.Entity.__init__(self, molecule=molecule, substance_form=substance_form, remaining=remaining, shelf_life=shelf_life)
 
     # Определяем метод, который показывает заканчивающиеся в ближайшие 180 дней сроки годности молекул
-    def upcoming_180_days():
+    @classmethod
+    def upcoming_180_days(cls):
         with db_session:
-            results = select((molecule, f'Срок годности до: {molecule_property.shelf_life}')
-                                for molecule in Molecule
-                                for molecule_property in molecule.properties
-                                if molecule_property.shelf_life < datetime.today() + timedelta(days=180))
-            return (list(results))
+            return [x.molecule for x in cls.select(lambda p: p.shelf_life < datetime.today() + timedelta(days=180))]
 
     # Определяем метод, который принимает дату и выводит молекулы до этой даты
-    def molecules_before_date(year, month, day):
+    @classmethod
+    def molecules_before_date(cls, year, month, day):
         with db_session:
-            results = select((molecule, f'Срок годности до: {molecule_property.shelf_life}')
-                                for molecule in Molecule
-                                for molecule_property in molecule.properties
-                                if molecule_property.shelf_life < datetime(year, month, day))
-            return (list(results))         
+            return [x.molecule for x in cls.select(lambda p: p.shelf_life < datetime(year, month, day))]      
 
     # Определяем метод, который вернёт все молекулы определённой формы
-    def find_form(subst_form):
+    @classmethod
+    def find_form(cls, subst_form):
         with db_session:
-            result = select((molecule, f'Форма: {molecule_property.substance_form}')
-                                for molecule in Molecule
-                                for molecule_property in molecule.properties
-                                if molecule_property.substance_form == subst_form)
-            for i in result:
-                yield i
+            return [x.molecule for x in cls.select(lambda p: p.substance_form == subst_form)]
     
     # Определяем метод, который вернёт все молекулы меньше определённого порога по весу
-    def less_than(remain):
+    @classmethod
+    def less_than(cls, remain):
         with db_session:
-            result = select((molecule, f'Остаток по весу: {molecule_property.remaining}')
-                                for molecule in Molecule
-                                for molecule_property in molecule.properties
-                                if molecule_property.remaining < remain)
-            for i in result:
-                yield i
+            return [x.molecule for x in cls.select(lambda p: p.remaining < remain)]
      
      # Определяем метод, который вернёт полное описание молекулы
     @property
-    def get_molecule_description(self):
+    def molecule_description(self):
         with db_session:
-            res_1 = {'Форма субстанции': self.substance_form for self in Molecule_Property.select(lambda i: i.id == self.id)}
-            res_2 = {'Остаток': str(int(self.remaining)) for self in Molecule_Property.select(lambda i: i.id == self.id)}
-            res_3 = {'Срок годности': self.shelf_life.strftime('%d.%m.%Y') for self in Molecule_Property.select(lambda i: i.id == self.id)}
-        return res_1, res_2, res_3
+            res_1 = {'Форма субстанции': self.substance_form}
+            res_2 = {'Остаток': str(int(self.remaining))}
+            res_3 = {'Срок годности': self.shelf_life.strftime('%d.%m.%Y')}
+        return {**res_1, **res_2, **res_3}
 
 class Molecule_Structure(db.Entity):
     molecule = Required(Molecule)
@@ -135,6 +120,29 @@ class Storage(db.Entity):
     def __init__(self, molecule, place='No information', supplier='No information'):
         db.Entity.__init__(self, molecule=molecule, place=place, supplier=supplier)
 
+class Reaction(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    molecules = Set('MoleculeReaction')
+    metadata = Optional(Json)
+    reaction_indexs = Set('ReactionIndex')
+
+    def __init__(self, structure):
+        if ReactionIndex.exists(signature=bytes(~structure)):
+            raise ValueError('That molecule is already in the database >:-(')
+        Reactants=structure.reactants
+        Products=structure.products
+
+class MoleculeReaction(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    reaction = Required(Reaction)
+    molecule = Required(Molecule)
+    product = Optional(bool)
+
+class ReactionIndex(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    reaction = Required(Reaction)
+    signature = Optional(Json)
+    bit_array = Optional(str)
 
 db.bind('sqlite', 'moleculesDB.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
@@ -144,11 +152,29 @@ with SDFRead('logBB.sdf') as f:
     data_molecules = [r for r in f]
 
 with db_session:
-    # db.Molecule(data_molecules[1], organic_molecule=True, substance_form='Powder', remaining='480',
+    # Тест работы метода find_molecule_in_db()
+    # data_molecules[3].canonicalize
+    # print(Molecule.find_molecule_in_db(data_molecules[2]))
+
+    # Добавить молекулу из logBB.sdf
+    # db.Molecule(data_molecules[3], organic_molecule=True, substance_form='Powder', remaining='480',
     #                     shelf_life='01.05.2023', place='18', supplier='EcoPharm')
 
     # db.Molecule[1].add_info(substance_form='Solution', remaining=500.0, shelf_life='01.01.2023')
     # print(db.Molecule[1].structure.get_structure())
     # print(db.Molecule[1]._structure)
 
-    print(db.Molecule[2].molecule_description)
+    # Тест работы метода description()
+    # print(db.Molecule[1].description)
+
+    # Тест работы метода upcoming_180_days()
+    # print(Molecule_Property.upcoming_180_days())
+
+    # Тест работы метода molecules_before_date()
+    # print(Molecule_Property.molecules_before_date(2023, 4, 30))
+
+    # Тест работы метода find_form()
+    # print(Molecule_Property.find_form('Solution'))
+
+    # Тест работы метода less_than()
+    print(Molecule_Property.less_than(499))
