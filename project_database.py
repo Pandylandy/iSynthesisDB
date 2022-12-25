d0cd984ef743925f25ed4ffb5f97e959d3698dba
Rodion Vladimirov <r.vladimirov@outlook.com>
2022-12-25 14:48:22 +0300

    Changes to be committed: 	modified:   project_database.py     added classes for work with reactions,     added method that returns all structures of one molecule

--- project_database.py
+++ project_database.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
 from pony.orm import *
 from datetime import *
-from CGRtools.files import SDFRead
+from CGRtools.files import SDFRead, RDFRead, SMILESRead
+from CGRtools.containers import ReactionContainer
 from pickle import dumps, loads
-import time
 
 db = Database()
 
@@ -19,11 +19,11 @@ class Molecule(db.Entity):
     reactions = Set('MoleculeReaction')
 
     def __init__(self, structure, organic_molecule=True, substance_form='No information', remaining=None,
-                        shelf_life='01.01.1900', place='No information', supplier='No information'):
+                        shelf_life='01.01.1900', place='No information', supplier='No information', canonical_structure='No information'):
         if Molecule_Structure.exists(signature=bytes(structure)):
             raise ValueError('That molecule is already in the database >:-(')
         db.Entity.__init__(self, name = str(structure), organic_molecule = organic_molecule)
-        Molecule_Structure(molecule=self, signature=bytes(structure), data=structure)
+        Molecule_Structure(molecule=self, signature=bytes(structure), data=structure, canonical_structure=canonical_structure)
         Molecule_Property(molecule=self, substance_form=substance_form, remaining=remaining, shelf_life=shelf_life)
         Storage(molecule=self, place=place, supplier=supplier)
         print('Molecule added')
@@ -37,9 +37,10 @@ class Molecule(db.Entity):
         return [x.molecule_description for x in self.properties]
 
     # Определяем метод, в который передаём молекулу в каноничном виде и находим её в базе данных
+    @staticmethod
     def find_molecule_in_db(any_molecule):
         with db_session:
-            result =  Molecule_Structure.get(signature=bytes(any_molecule))
+            result = Molecule_Structure.get(signature=bytes(any_molecule))
             if result:
                 return result.molecule
 
@@ -48,6 +49,15 @@ class Molecule(db.Entity):
                         shelf_life='01.01.1900', place='No information', supplier='No information'):
         Molecule_Property(molecule=self, substance_form=substance_form, remaining=remaining, shelf_life=shelf_life)
         Storage(molecule=self, place=place, supplier=supplier)
+
+    # Метод, который возвращает все структуры одной молекулы
+    @property
+    def structures(self):
+        structures = []
+        with db_session:
+            for structure in self.structure:
+                structures.append(structure)
+        return structures
 
 class Molecule_Property(db.Entity):
     molecule = Required(Molecule)
@@ -101,10 +111,11 @@ class Molecule_Structure(db.Entity):
     signature = Required(bytes)
     bit_array = Optional(str)
     data = Required(bytes)
+    canonical_structure = Optional(str)
 
-    def __init__(self, molecule, data, signature):
+    def __init__(self, molecule, data, signature, canonical_structure):
         data = dumps(data)
-        db.Entity.__init__(self, molecule=molecule, signature=signature, data=data)
+        db.Entity.__init__(self, molecule=molecule, signature=signature, data=data, canonical_structure=canonical_structure)
 
     @property
     def structure(self):
@@ -127,11 +138,43 @@ class Reaction(db.Entity):
     reaction_indexs = Set('ReactionIndex')
 
     def __init__(self, structure):
+        if not isinstance(structure, ReactionContainer):
+            raise ValueError('It is not a reaction >:-(')
         if ReactionIndex.exists(signature=bytes(~structure)):
-            raise ValueError('That molecule is already in the database >:-(')
-        Reactants=structure.reactants
-        Products=structure.products
+            raise ValueError('That reaction is already in the database >:-(')
+        if not structure.reactants:
+            raise ValueError('That reaction have no any reactants >:-(')
+        if not structure.products:
+            raise ValueError('That reaction have no any products >:-(')
+        reactants=structure.reactants
+        products=structure.products
+        db.Entity.__init__(self)
+        for structures,p in ((products, True), (reactants, False)):
+            for m in structures:
+                mol = Molecule.find_molecule_in_db(m)
+                if mol:
+                    MoleculeReaction(reaction=self, molecule=mol, product=p)
+                else:
+                    mol = Molecule(m)
+                    MoleculeReaction(reaction=self, molecule=mol, product=p)
+        ReactionIndex(reaction=self, signature=bytes(~structure))
 
+     # Определяем метод, в который передаём реакцию и находим её объект в базе данных
+    @staticmethod
+    def find_reaction_in_db(any_reaction):
+        with db_session:
+            result = ReactionIndex.get(signature=bytes(~any_reaction))
+            if result:
+                return result.reaction
+
+    # Метод возвращает ReactionContainer
+    @property
+    def structure(self):
+        res_1 = [(i.molecule._structure, i.product) for i in self.molecules]
+        list_of_products = [i[0] for i in res_1 if i[1] == True]
+        list_of_reactants = [i[0] for i in res_1 if i[1] == False]
+        return ReactionContainer(reactants=list_of_reactants, products=list_of_products)
+        
 class MoleculeReaction(db.Entity):
     id = PrimaryKey(int, auto=True)
     reaction = Required(Reaction)
@@ -141,7 +184,7 @@ class MoleculeReaction(db.Entity):
 class ReactionIndex(db.Entity):
     id = PrimaryKey(int, auto=True)
     reaction = Required(Reaction)
-    signature = Optional(Json)
+    signature = Optional(bytes)
     bit_array = Optional(str)
 
 db.bind('sqlite', 'moleculesDB.sqlite', create_db=True)
@@ -151,14 +194,21 @@ data_molecules = []
 with SDFRead('logBB.sdf') as f:
     data_molecules = [r for r in f]
 
+data_reactions = []
+with RDFRead('example.rdf') as f:
+    data_reactions = [r for r in f]
+reaction = next(SMILESRead('untitled.txt'))
+
+reaction_bug = ReactionContainer(reaction.products, []) 
+
 with db_session:
     # Тест работы метода find_molecule_in_db()
-    # data_molecules[3].canonicalize
+    # data_molecules[3].canonicalize()
     # print(Molecule.find_molecule_in_db(data_molecules[2]))
 
     # Добавить молекулу из logBB.sdf
     # db.Molecule(data_molecules[3], organic_molecule=True, substance_form='Powder', remaining='480',
-    #                     shelf_life='01.05.2023', place='18', supplier='EcoPharm')
+    #                     shelf_life='01.05.2023', place='18', supplier='EcoPharm', canonical_structure='No')
 
     # db.Molecule[1].add_info(substance_form='Solution', remaining=500.0, shelf_life='01.01.2023')
     # print(db.Molecule[1].structure.get_structure())
@@ -177,4 +227,19 @@ with db_session:
     # print(Molecule_Property.find_form('Solution'))
 
     # Тест работы метода less_than()
-    print(Molecule_Property.less_than(499))
+    # print(Molecule_Property.less_than(499))
+
+    # Тест добавления реакции в базу
+    # db.Reaction(reaction_bug)
+
+    # Тест проверки наличия реакции в базе
+    # print(Reaction.find_reaction_in_db(reaction))
+
+    # Тест проверки работы метода Reaction.structure()
+    # print(db.Reaction[2].structure)
+
+    # Скрипт для добавления ещё одной структуры к уже имеющейся молекуле
+    # Molecule_Structure(molecule=Molecule.get(id=1), signature=bytes(data_molecules[3].canonicalize()), data=data_molecules[3].canonicalize(), canonical_structure='Yes')
+
+    # Тест работы метода structures
+    print(db.Molecule[1].structures)
