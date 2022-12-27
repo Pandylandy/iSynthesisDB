1e27475dfaef6561206934e9ee3a6e08f242f5ab
Rodion Vladimirov <r.vladimirov@outlook.com>
2022-12-27 14:09:16 +0300

    Changes to be committed: 	modified:   project_database.py 	modified:   work_in_database.py

--- project_database.py
+++ project_database.py
@@ -1,12 +1,12 @@
+from chython import SDFRead, RDFRead, SMILESRead
+from chython.containers import ReactionContainer
+from datetime import datetime, timedelta
 from decimal import Decimal
-from pony.orm import *
-from datetime import *
-from CGRtools.files import SDFRead, RDFRead, SMILESRead
-from CGRtools.containers import ReactionContainer
 from pickle import dumps, loads
+from pony.orm import Database, PrimaryKey, Required, Set, Optional, Json, db_session
+
 
 db = Database()
-
 
 class Molecule(db.Entity):
     """Full information about every molecule in laboratory"""
@@ -39,10 +39,9 @@ class Molecule(db.Entity):
     # Определяем метод, в который передаём молекулу в каноничном виде и находим её в базе данных
     @staticmethod
     def find_molecule_in_db(any_molecule):
-        with db_session:
-            result = Molecule_Structure.get(signature=bytes(any_molecule))
-            if result:
-                return result.molecule
+        result = Molecule_Structure.get(signature=bytes(any_molecule))
+        if result:
+            return result.molecule
 
     # Определяем метод, который обновляет информацию по имеющейся в базе данных молекуле
     def add_info(self, substance_form='No information', remaining=None,
@@ -54,6 +53,23 @@ class Molecule(db.Entity):
     @property
     def structures(self):
         return [structure for structure in self.structure]
+
+    # Внутренняя функция, недоступная извне для двух последующих методов ниже
+    def __find_reactions_by_molecule(cls, molecule, product):
+        finding_mol = cls.find_molecule_in_db(molecule)
+        if finding_mol:
+            reactions = [i.reaction for i in finding_mol.reactions if i.molecule == finding_mol and i.product == product]
+        return reactions
+
+    # Метод, который возвращает все реакции, в которых передаваемая молекула является продуктом
+    @classmethod
+    def find_reactions_with_molecule_as_product(cls, molecule):
+        return cls.__find_reactions_by_molecule(molecule, product=True)
+
+    # Метод, который возвращает все реакции, в которых передаваемая молекула является реактантом
+    @classmethod
+    def find_reactions_with_molecule_as_reactant(cls, molecule):
+        return cls.__find_reactions_by_molecule(molecule, product=False)
 
 class Molecule_Property(db.Entity):
     molecule = Required(Molecule)
@@ -71,34 +87,29 @@ class Molecule_Property(db.Entity):
     # Определяем метод, который показывает заканчивающиеся в ближайшие 180 дней сроки годности молекул
     @classmethod
     def upcoming_180_days(cls):
-        with db_session:
-            return [x.molecule for x in cls.select(lambda p: p.shelf_life < datetime.today() + timedelta(days=180))]
+        return [x.molecule for x in cls.select(lambda p: p.shelf_life < datetime.today() + timedelta(days=180))]
 
     # Определяем метод, который принимает дату и выводит молекулы до этой даты
     @classmethod
     def molecules_before_date(cls, year, month, day):
-        with db_session:
-            return [x.molecule for x in cls.select(lambda p: p.shelf_life < datetime(year, month, day))]      
+        return [x.molecule for x in cls.select(lambda p: p.shelf_life < datetime(year, month, day))]      
 
     # Определяем метод, который вернёт все молекулы определённой формы
     @classmethod
     def find_form(cls, subst_form):
-        with db_session:
-            return [x.molecule for x in cls.select(lambda p: p.substance_form == subst_form)]
+        return [x.molecule for x in cls.select(lambda p: p.substance_form == subst_form)]
     
     # Определяем метод, который вернёт все молекулы меньше определённого порога по весу
     @classmethod
     def less_than(cls, remain):
-        with db_session:
-            return [x.molecule for x in cls.select(lambda p: p.remaining < remain)]
+        return [x.molecule for x in cls.select(lambda p: p.remaining < remain)]
      
      # Определяем метод, который вернёт полное описание молекулы
     @property
     def molecule_description(self):
-        with db_session:
-            res_1 = {'Форма субстанции': self.substance_form}
-            res_2 = {'Остаток': str(int(self.remaining))}
-            res_3 = {'Срок годности': self.shelf_life.strftime('%d.%m.%Y')}
+        res_1 = {'Форма субстанции': self.substance_form}
+        res_2 = {'Остаток': str(int(self.remaining))}
+        res_3 = {'Срок годности': self.shelf_life.strftime('%d.%m.%Y')}
         return {**res_1, **res_2, **res_3}
 
 class Molecule_Structure(db.Entity):
@@ -158,19 +169,20 @@ class Reaction(db.Entity):
      # Определяем метод, в который передаём реакцию и находим её объект в базе данных
     @staticmethod
     def find_reaction_in_db(any_reaction):
-        with db_session:
-            result = ReactionIndex.get(signature=bytes(~any_reaction))
-            if result:
-                return result.reaction
+        result = ReactionIndex.get(signature=bytes(~any_reaction))
+        if result:
+            return result.reaction
 
-    # Метод возвращает ReactionContainer
+    # Метод получает объект класса Reaction и возвращает ReactionContainer этого класса
     @property
     def structure(self):
         res_1 = [(i.molecule._structure, i.product) for i in self.molecules]
+        print(res_1)
+        print(self.molecules)
         list_of_products = [i[0] for i in res_1 if i[1] == True]
         list_of_reactants = [i[0] for i in res_1 if i[1] == False]
         return ReactionContainer(reactants=list_of_reactants, products=list_of_products)
-        
+
 class MoleculeReaction(db.Entity):
     id = PrimaryKey(int, auto=True)
     reaction = Required(Reaction)
@@ -185,57 +197,3 @@ class ReactionIndex(db.Entity):
 
 db.bind('sqlite', 'moleculesDB.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
-
-data_molecules = []
-with SDFRead('logBB.sdf') as f:
-    data_molecules = [r for r in f]
-
-data_reactions = []
-with RDFRead('example.rdf') as f:
-    data_reactions = [r for r in f]
-reaction = next(SMILESRead('untitled.txt'))
-
-reaction_bug = ReactionContainer(reaction.products, []) 
-
-with db_session:
-    # Тест работы метода find_molecule_in_db()
-    # data_molecules[3].canonicalize()
-    # print(Molecule.find_molecule_in_db(data_molecules[2]))
-
-    # Добавить молекулу из logBB.sdf
-    # db.Molecule(data_molecules[3], organic_molecule=True, substance_form='Powder', remaining='480',
-    #                     shelf_life='01.05.2023', place='18', supplier='EcoPharm', canonical_structure='No')
-
-    # db.Molecule[1].add_info(substance_form='Solution', remaining=500.0, shelf_life='01.01.2023')
-    # print(db.Molecule[1].structure.get_structure())
-    # print(db.Molecule[1]._structure)
-
-    # Тест работы метода description()
-    # print(db.Molecule[1].description)
-
-    # Тест работы метода upcoming_180_days()
-    # print(Molecule_Property.upcoming_180_days())
-
-    # Тест работы метода molecules_before_date()
-    # print(Molecule_Property.molecules_before_date(2023, 4, 30))
-
-    # Тест работы метода find_form()
-    # print(Molecule_Property.find_form('Solution'))
-
-    # Тест работы метода less_than()
-    # print(Molecule_Property.less_than(499))
-
-    # Тест добавления реакции в базу
-    # db.Reaction(reaction_bug)
-
-    # Тест проверки наличия реакции в базе
-    # print(Reaction.find_reaction_in_db(reaction))
-
-    # Тест проверки работы метода Reaction.structure()
-    # print(db.Reaction[2].structure)
-
-    # Скрипт для добавления ещё одной структуры к уже имеющейся молекуле
-    # Molecule_Structure(molecule=Molecule.get(id=1), signature=bytes(data_molecules[3].canonicalize()), data=data_molecules[3].canonicalize(), canonical_structure='Yes')
-
-    # Тест работы метода structures
-    print(db.Molecule[1].structures)
